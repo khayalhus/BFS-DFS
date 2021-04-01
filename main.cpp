749192b04163725652469b329dfd57e028e862fc
Khayal H <43758778+khayalhus@users.noreply.github.com>
2021-04-01 15:13:29 +0400

    Implement BFS traversal and fix tree
    Stop creating unnecessary nodes to free memory
    Implement the output format

--- main.cpp
+++ main.cpp
@@ -2,6 +2,7 @@
 #include<fstream> // ifstream, ofstream
 #include<time.h> // clock_t, clock, CLOCKS_PER_SEC
 #include<string> // string, getline
+#include<queue>
 
 using namespace std;
 
@@ -9,25 +10,39 @@ class Node {
     public:
     char letter;
     Node** childs;  // child pointer holder holder
-    int rank;   // level in tree
+    int rank;  // level in tree
     int childSize; // amount of children nodes
     int* combination;
 
     Node(char letter, int rank) {
         this->letter = letter;
-        this->childs = new Node * [10];
+        this->childs = new Node * [10 - rank];
         this->rank = rank;
         this->childSize = 0;
     }
     void createCombination(int* parentCombination, int uniqueLetterAmount, int newIndex) {
+        int possibilities[10] = {1, 1, 1, 1, 1, 1, 1, 1, 1, 1};
         this->combination = new int[uniqueLetterAmount];
+
         for (int i = 0; i < uniqueLetterAmount; i++) {
-            if (i < rank) {
+            if (i < rank - 1) {
                 this->combination[i] = parentCombination[i];
+                possibilities[parentCombination[i]] = 0;
             } else {
                 this->combination[i] = -1;
-                if (i == rank) {
-                    this->combination[i] = newIndex;
+                if (i == rank - 1) {
+                    int k = 0;
+                    for (int j = 0; j < 10; j++) {
+                        if (possibilities[j] == 1) {
+                            if(k == newIndex) {
+                                this->combination[i] = j;
+                                break;
+                            } else {
+                                k++;
+                            }
+                            
+                        }
+                    }
                 }
             }
         }
@@ -39,8 +54,42 @@ class Node {
         }
         cout << '\n';
     }
-    void checkConstraint() {
-        // chekku shite kadasai
+
+    int checkSum(string st, char * uniqueLetters, int uniqueLetterAmount) {
+        int sum = 0;
+        for (int i = st.length() - 1, j = 0; i >= 0; i--, j++) {
+            int pow = 1;
+            for (int k = 0; k < j; k++) {
+                pow *= 10;
+            }
+            int k = 0;
+            int charIndex = -1;
+            while(k < uniqueLetterAmount) {
+                if (toupper(uniqueLetters[k]) == st.at(i)) {
+                    charIndex = k;
+                }
+                k++;
+            }
+            if (this->combination[charIndex] == -1) {
+                return 0;
+            }
+            sum += pow * this->combination[charIndex];
+        }
+        return sum;
+    }
+
+    int * checkConstraint(string augend, string addend, string sum, char * uniqueLetters, int uniqueLetterAmount) {
+        /*
+        int sumAugend = checkSum(augend, uniqueLetters, uniqueLetterAmount);
+        int sumAddend = checkSum(addend, uniqueLetters, uniqueLetterAmount);
+        int sumSum = checkSum(sum, uniqueLetters, uniqueLetterAmount);
+        if (sumAugend == 0 || sumAddend == 0 || sumSum == 0) {
+            return NULL;
+        }
+        if (sumAugend + sumAddend == sumSum) {
+            return this->combination;
+        }*/
+        return NULL;
     }
 };
 
@@ -59,20 +108,19 @@ class Tree {
         }
         this->uniqueLetterAmount = uniqueLetterAmount;
         this->uniqueLetters = uniqueLetters;
-        
-        recursiveCreate(this->root, 0);
-        this->root->combination[0] = -1;
+        ofstream outFile;
+        recursiveCreate(this->root);
     }
 
-    void recursiveCreate(Node* currentNode, int index) {
-        if (index == uniqueLetterAmount) {
+    void recursiveCreate(Node* currentNode) {
+        if (currentNode->rank == uniqueLetterAmount) {
             return;
         }
-        for (int i = 0; i < 10; i++) {
-            currentNode->childs[i] = new Node(this->uniqueLetters[index], index);
+        for (int i = 0; i < 10 - currentNode->rank; i++) {
+            currentNode->childs[i] = new Node(this->uniqueLetters[currentNode->rank+1], currentNode->rank+1);
             currentNode->childs[i]->createCombination(currentNode->combination, uniqueLetterAmount, i);
             //currentNode->childs[i]->print(uniqueLetterAmount);
-            recursiveCreate(currentNode->childs[i], index+1);
+            recursiveCreate(currentNode->childs[i]);
         }
     }
 };
@@ -101,7 +149,7 @@ int main (int argc, char** argv) {
 	}
 	
 	ifstream inFile;
-	inFile.open(augend.append(" ").append(addend).append(" ").append(sum).append(".txt"));
+	inFile.open(augend + " " + addend + " " + sum + ".txt");
 
 	if (!inFile){
 		cerr << "ERROR: Input file cannot be opened!" << endl;
@@ -127,8 +175,32 @@ int main (int argc, char** argv) {
 
     Tree * mytree = new Tree(uniqueLetterAmount, uniqueLetters);
 
+    cout << ( (float) clock() - runtime ) / CLOCKS_PER_SEC << endl;
+
+    int visitedNodeAmount = 0;
+
+    int * answer = new int[uniqueLetterAmount];
+
     if (search_type.compare("BFS") == 0) {
-        
+        queue<Node*> q;
+        Node* traverser;
+        q.push(mytree->root);
+        while(q.empty() == false) {
+            traverser = q.front();
+            //traverser->print(uniqueLetterAmount);
+            answer = traverser->checkConstraint(augend, addend, sum, uniqueLetters, uniqueLetterAmount);
+            visitedNodeAmount++;
+            q.pop();
+            if (answer != NULL) {
+                // found answer
+                break;
+            }
+            if (traverser->rank != uniqueLetterAmount) {
+                for (int i = 0; i < 10 - traverser->rank; i++) {
+                    q.push(traverser->childs[i]);
+                }
+            }
+        }
     } else if (search_type.compare("DFS") == 0) {
 
     } else {
@@ -137,9 +209,17 @@ int main (int argc, char** argv) {
     }
 
     cout << "Algorithm: " << search_type << endl;
-    cout << "Number of visited nodes: " << endl;
+    cout << "Number of visited nodes: " << visitedNodeAmount << endl;
     cout << "Maximum number of nodes kept in the memory: " << endl;
-    cout << "Solution: " << endl;
+    cout << "Solution: ";
+    for (int i = 0; i < uniqueLetterAmount; i++) {
+        cout << (char)toupper(uniqueLetters[i]) << ": " << answer[i];
+        if (i + 1 != uniqueLetterAmount) {
+            cout << ", ";
+        } else {
+            cout << endl;
+        }
+    }
 
     ofstream outFile;
 
@@ -149,6 +229,33 @@ int main (int argc, char** argv) {
 		cerr << "ERROR: Output file cannot be opened!" << endl;
 		exit(1);
 	}
+
+    outFile << '\t'; // initial empty tab
+
+    for (int i = 0; i < 10; i++) {
+        outFile << i;
+        if (i != 9) {
+            outFile << '\t';
+        } else {
+            outFile << endl;
+        }
+    }
+
+    for (int i = 0; i < uniqueLetterAmount; i++) {
+        outFile << uniqueLetters[i] << '\t';
+        for (int j = 0; j < 10; j++) {
+            if (j == answer[i]) {
+                outFile << 1;
+            } else {
+                outFile << '.';
+            }
+            if (j + 1 != 10) {
+                outFile << '\t';
+            } else {
+                outFile << endl;
+            }
+        }
+    }
 
     outFile.close();
 
